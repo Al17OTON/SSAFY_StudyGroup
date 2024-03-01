f6fe953aa1cd4c508d0ee1c7fbbb082f5e65d9da
Jeon-YJ1004 <dudwn5374@naver.com>
2024-03-01 17:54:46 +0900

    change 원판

--- 전영주/Week07/BOJ_17822_원판돌리기.java
+++ 전영주/Week07/BOJ_17822_원판돌리기.java
@@ -8,6 +8,7 @@ public class BOJ_17822_원판돌리기 {
     static int[][]arr;
     static int[][]rotateInput;
     static double avg;
+    static int []firstPoint;
     public static void main(String[] args) throws Exception {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st=new StringTokenizer(br.readLine());
@@ -15,12 +16,12 @@ public class BOJ_17822_원판돌리기 {
         m=Integer.parseInt(st.nextToken());
         t=Integer.parseInt(st.nextToken());
         arr=new int[n][m];
+        firstPoint=new int[n];
         rotateInput=new int[t][3];
         for (int i = 0; i < n; i++) {
             st=new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
-                arr[i][j]=Integer.parseInt(st.nextToken());
-                
+                arr[i][j]=Integer.parseInt(st.nextToken());             
             }
         }
         for (int i = 0; i < t; i++) {
@@ -29,15 +30,15 @@ public class BOJ_17822_원판돌리기 {
             int x=Integer.parseInt(st.nextToken());//몇번째 원판
             int d=Integer.parseInt(st.nextToken());//시계0 반시계1
             int k=Integer.parseInt(st.nextToken());//k 칸
-            if(d==0)k=n-k;
-            print(arr);
-            rotateSimul(x-1,k);
-            print(arr);
+            if(d==0)k=m-k;
+            for (int j = x; j <=n; j+=x) {
+				rotateSimul(j-1,k);
+			}
+
             if(!eraseSimul()) {
             	//평균 구하기
             	makeValueAverage();
             }
-            print(arr);
             
         }
         int answer=0;
@@ -48,57 +49,48 @@ public class BOJ_17822_원판돌리기 {
         }
         System.out.println(answer);
     }
-     static void print(int [][]v) {
-            for (int i = 0; i < v.length; i++) {
-                for (int j = 0; j < v[0].length; j++) {
-                    System.out.print(v[i][j]+" ");
-                }
-                System.out.println();
-            }
-            System.out.println();
-        }
+
     //반시계만 고려
     private static void rotateSimul(int x,int k) {
-        int temp=arr[x][0];
-        for (int i = 0; i < n-k; i++) {
-            arr[x][i]=arr[x][i+k];
-        }
-        
-        for (int i = n-k; i < n-1; i++) {
-			arr[x][i]=arr[x][i+n-k];//4
-		}
-         arr[x][n-k]=temp;
+        if(firstPoint[x]+k>=m)firstPoint[x]+=k-m;
+        else firstPoint[x]+=k;
     }
+    
+   private static int checkBoundary(int i,int index) {
+		if(firstPoint[i]+index>=m)return firstPoint[i]+index-(m);
+        else return firstPoint[i]+index;
+	} 
+    
     private static boolean eraseSimul() {
     	boolean flag=false;
-    	avg=0;
+
         int [][]temp=new int[n][];
         for (int i = 0; i < n; i++) {
             temp[i]=Arrays.copyOf(arr[i], m);
         }
         //i,1
         for (int i = 0; i < n; i++) {
-            
-            if(temp[i][0]==temp[i][1]&&temp[i][0]!=0) {
+            if(temp[i][0]==0)continue;
+            if(temp[i][0]==temp[i][1]) {
                 arr[i][0]=0;
                 arr[i][1]=0;
                 flag=true;
             }
-            if(temp[i][0]==temp[i][m-1]&&temp[i][0]!=0) {
+            if(temp[i][0]==temp[i][m-1]) {
                 arr[i][0]=0;
                 arr[i][m-1]=0;
                 flag=true;
             }
         }
-        //같은 행 체크
+        //같은 행 
         for (int i =0; i < n; i++) {
             for (int j = 1; j <m-1; j++) {
             	
-                if(temp[i][j]==temp[i][j+1]&&temp[i][j]!=0) {
+            	if(temp[i][j]==0)continue;
+                if(temp[i][j]==temp[i][j+1]) {
                 arr[i][j]=0;
                 arr[i][j+1]=0;
                 flag=true;
-                
                 }
             }
         }
@@ -106,9 +98,12 @@ public class BOJ_17822_원판돌리기 {
         //같은 열 체크
         for (int i =0; i < n-1; i++) {
             for (int j = 0; j <m; j++) {
-                if(temp[i][j]==temp[i+1][j]&&temp[i][j]!=0) {
-                arr[i][j]=0;
-                arr[i+1][j]=0;
+            	int a=checkBoundary(i, j);
+            	int b=checkBoundary(i+1, j);
+            	if(temp[i][checkBoundary(i, j)]==0)continue;
+                if(temp[i][checkBoundary(i, j)]==temp[i+1][checkBoundary(i+1, j)]) {
+                arr[i][checkBoundary(i, j)]=0;
+                arr[i+1][checkBoundary(i+1, j)]=0;
                 flag=true;
                 }
             }
@@ -120,7 +115,7 @@ public class BOJ_17822_원판돌리기 {
     }
     private static void sumArr() {
     	avg=0;
-    	int count=0;
+    	double count=0;
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
 				avg+=arr[i][j];
