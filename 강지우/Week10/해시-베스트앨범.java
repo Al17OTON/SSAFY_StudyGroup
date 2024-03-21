6ec1714bba1f19d0cbb05861cb4d0230d507815c
jiu <118248199+jiu6525@users.noreply.github.com>
2024-03-21 20:16:42 +0900

    0321

---
+++ 강지우/Week10/해시-베스트앨범.java
@@ -0,0 +1,57 @@
+import java.util.*;
+class Solution {
+    public static void main(String[] args) {
+        String[] a = {"classic", "pop", "classic", "classic", "pop"};
+        int[] b = {500, 600, 150, 800, 2500};
+        solution(a,b);
+    }
+    public static int[] solution(String[] genres, int[] plays) {
+        List<Integer> answerList = new ArrayList<>();
+        Map<String, Integer> totalPlays = new HashMap<>();
+        Map<String, List<Map<Integer, Integer>>> playList = new HashMap<>();
+
+        //playList 세팅
+        for (String genre : genres) {
+            playList.put(genre, new ArrayList<>());
+        }
+
+        //playList 값 넣기
+        for (int i = 0; i < genres.length; i++) {
+            totalPlays.put(genres[i], totalPlays.getOrDefault(genres[i], 0) + plays[i]);
+            Map<Integer, Integer> songInfo = new HashMap<>();
+            songInfo.put(i, plays[i]);
+            playList.get(genres[i]).add(songInfo);
+        }
+        
+        // 각 요소에 대한(재생수)에 따른 정렬
+        for (String genre : genres) {
+            List<Map<Integer, Integer>> list = playList.get(genre);
+            list.sort((o1, o2) -> o2.values().iterator().next().compareTo(o1.values().iterator().next()));
+
+        }
+//        {pop=[{1=600}, {4=2500}], classic=[{0=500}, {2=150}, {3=800}]}
+//        {pop=[{4=2500}, {1=600}], classic=[{3=800}, {0=500}, {2=150}]}
+
+        // 총합순이 높은걸 넣기
+        List<String> genresSortedByPlays = new ArrayList<>(totalPlays.keySet());
+        genresSortedByPlays.sort((o1, o2) -> totalPlays.get(o2) - totalPlays.get(o1));
+
+        for (String genre : genresSortedByPlays) {
+            List<Map<Integer, Integer>> list = playList.get(genre);
+            int count = 0;
+            for (Map<Integer, Integer> songInfo : list) {
+                answerList.add(songInfo.keySet().iterator().next());
+                count++;
+                if (count >= 2)
+                    break;
+            }
+        }
+
+        int[] answer = new int[answerList.size()];
+        for (int i = 0; i < answerList.size(); i++) {
+            answer[i] = answerList.get(i);
+        }
+
+        return answer;
+    }
+}
---
+++ 강지우/Week10/해시-의상.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int solution(String[][] clothes) {
+        Map<String, Integer> map = new HashMap<>();
+        int answer = 1;
+
+        for(int i = 0; i < clothes.length; i++) {
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
+        }
+
+        for(String key : map.keySet()) {
+            answer *= (map.get(key) + 1);   // 지금 옷의 경우의 수와 안입는 옷 * 누적
+        }
+
+        return answer -= 1; // 아무것도 입지 않는경우
+    }
+}
